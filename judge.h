ea9e3f1ec15c0daea803fe7859dbc94a8dc43577
arashzaki <67689645+arashzaki@users.noreply.github.com>
2020-07-02 15:18:16 +0430

    Create judge.h

---
+++ judge.h
@@ -0,0 +1,4 @@
+String *LogJudge(String *log,int index,int errorType);
+int ValidTestCases(String *base);
+int CompareTwoFileData(String *path1,String *path2);
+double Judge(String *programPath,String *testCasesBase,float miliSeconds);
